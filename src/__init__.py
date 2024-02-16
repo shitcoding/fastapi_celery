8c5af4d38d00fa8020b118b3a0089260c36e4c4b
Vasiliy Ivanov <shitcoder@pm.me>
2024-02-16 17:37:58 +0300

    Add users router to app

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,10 @@ from fastapi import FastAPI
 def create_app() -> FastAPI:
     app = FastAPI()
 
+    from src.users import users_router
+
+    app.include_router(users_router)
+
     @app.get('/')
     async def root():
         return {'message': 'Well hello motherfucka'}
